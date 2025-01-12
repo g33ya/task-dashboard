b4d2b23d6bf73e785891f475ca3aae6ce1c18299
g33ya <gscozzaro2004@gmail.com>
2025-01-11 23:13:29 -0500

    Created task checkbox component, added to table component

---
+++ app/components/checkbox.js
@@ -0,0 +1,8 @@
+export function TaskCheckbox() {
+    return (
+            <label htmlFor="completed">
+                <input type="checkbox" id="completed" name="completed" value="yes" />
+            </label>
+        
+    );
+}
