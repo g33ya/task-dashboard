cecdbe72da5a4d8e2359b6ba9fe0a784d202120c
g33ya <gscozzaro2004@gmail.com>
2025-01-12 00:14:33 -0500

    Changed class -> className

--- app/components/table.js
+++ app/components/table.js
@@ -2,15 +2,15 @@ import { TableRow } from './tablerow.js';
 
 export function TaskTable() {
     return (
-        <table class="table-auto border-collapse border border-gray-300 w-full text-left">
-            <thead class="bg-gray-200">
+        <table className="table-auto border-collapse border border-gray-300 w-full text-left">
+            <thead className="bg-gray-200">
             <tr>
-                <th class="border border-gray-300 px-4 py-2">Completed</th>
-                <th class="border border-gray-300 px-4 py-2">Task Number</th>
-                <th class="border border-gray-300 px-4 py-2">Task Name</th>
-                <th class="border border-gray-300 px-4 py-2">Status</th>
-                <th class="border border-gray-300 px-4 py-2">Due Date</th>
-                <th class="border border-gray-300 px-4 py-2">Notes</th>
+                <th className="border border-gray-300 px-4 py-2">Completed</th>
+                <th className="border border-gray-300 px-4 py-2">Task Number</th>
+                <th className="border border-gray-300 px-4 py-2">Task Name</th>
+                <th className="border border-gray-300 px-4 py-2">Status</th>
+                <th className="border border-gray-300 px-4 py-2">Due Date</th>
+                <th className="border border-gray-300 px-4 py-2">Notes</th>
 
             </tr>
             </thead>
@@ -18,13 +18,13 @@ export function TaskTable() {
             
             <TableRow />
             
-            <tr class="bg-gray-100">
-                <td class="border border-gray-300 px-4 py-2">Holder</td>
-                <td class="border border-gray-300 px-4 py-2">2</td>
-                <td class="border border-gray-300 px-4 py-2">Review Basics</td>
-                <td class="border border-gray-300 px-4 py-2">In Progress</td>
-                <td class="border border-gray-300 px-4 py-2">1/17/25</td>
-                <td class="border border-gray-300 px-4 py-2">React/JS</td>
+            <tr className="bg-gray-100">
+                <td className="border border-gray-300 px-4 py-2">Holder</td>
+                <td className="border border-gray-300 px-4 py-2">2</td>
+                <td className="border border-gray-300 px-4 py-2">Review Basics</td>
+                <td className="border border-gray-300 px-4 py-2">In Progress</td>
+                <td className="border border-gray-300 px-4 py-2">1/17/25</td>
+                <td className="border border-gray-300 px-4 py-2">React/JS</td>
             </tr>
             </tbody>
         </table>
--- app/components/tablerow.js
+++ app/components/tablerow.js
@@ -17,10 +17,10 @@ export function TableRow() {
         <tr className={`bg-white ${isChecked ? 'line-through' : ''}`}>
             <TaskCheckbox isChecked={isChecked} handleCheckboxChange={handleCheckboxChange}/>
             <TableField fieldType="tasknum" />
-            <td class="border border-gray-300 px-4 py-2">Create simple JS website</td>
-            <td class="border border-gray-300 px-4 py-2">In Progress</td>
-            <td class="border border-gray-300 px-4 py-2">1/17/25</td>
-            <td class="border border-gray-300 px-4 py-2">Let's see what I can do!</td>
+            <td className="border border-gray-300 px-4 py-2">Create simple JS website</td>
+            <td className="border border-gray-300 px-4 py-2">In Progress</td>
+            <td className="border border-gray-300 px-4 py-2">1/17/25</td>
+            <td className="border border-gray-300 px-4 py-2">Let's see what I can do!</td>
         </tr>
     );
 }
