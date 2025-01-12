28e48a5cf297f320b1ea7d4d10d526f4fa0be477
g33ya <gscozzaro2004@gmail.com>
2025-01-12 15:57:38 -0500

    ListName component basic setup

---
+++ app/components/listname.js
@@ -0,0 +1,8 @@
+"use client"; // DOM
+import { useState } from 'react';
+
+export function ListName() {
+    return (
+        <h1>List Name</h1>
+    );
+}
--- app/page.js
+++ app/page.js
@@ -1,12 +1,15 @@
 //import Image from "next/image";
 import { TaskTable } from './components/table.js';
 import { SearchBar } from './components/searchbar.js';
+import { ListName } from './components/listname.js';
+
 
 
 export default function Home() {
   return (
     <div>
       <div className="bg-red-500 text-black p-4">TaskNest</div>
+      <ListName />
       <SearchBar />
     </div>      
   );
